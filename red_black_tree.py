a66dd435dce33243398079c725a6c1dadebc5b20
daberg <daberg@openmailbox.org>
2017-01-17 14:54:47 +0100

    Add assert and remove functions.

--- red_black_tree.py
+++ red_black_tree.py
@@ -39,8 +39,7 @@ class RedBlackTree():
 
     def rotate_left(self, node):
 
-        if node.right is self.nil:
-            return None
+        # Should check if node.right is not self.nil?
 
         other = node.right
         node.right = other.left
@@ -64,8 +63,7 @@ class RedBlackTree():
 
     def rotate_right(self, node):
 
-        if node.left is self.nil:
-            return None
+        # Should check if node.left is not self.nil?
 
         other = node.left
         node.left = other.right
@@ -99,7 +97,7 @@ class RedBlackTree():
             if key < curr.key:
                 curr = curr.left
 
-            else: # key > curr.key
+            else:   # key > curr.key
                 curr = curr.right
 
         if curr is self.nil:
@@ -113,24 +111,24 @@ class RedBlackTree():
                 parent.left = node
                 node.parent = parent
 
-            else: # key > parent.key
+            else:   # key > parent.key
                 parent.right = node
                 node.parent = parent
 
-            self.fix_insert(node)
+            self._fix_insert(node)
 
-        else: # curr.key == key
+        else:   # curr.key == key
 
             curr.data = data
 
-    def fix_insert(self, node):
-           
+    def _fix_insert(self, node):
+
         while node is not self.nil:
 
             parent = node.parent
             gparent = parent.parent
             uncle = gparent.right if gparent.left is parent else gparent.left
-            
+
             # Case: 1
             if parent is self.nil:
 
@@ -189,7 +187,7 @@ class RedBlackTree():
             if key < node.key:
                 node = node.left
 
-            else: # key > node.key
+            else:   # key > node.key
                 node = node.right
 
         if node is self.nil:
@@ -197,29 +195,337 @@ class RedBlackTree():
 
         else:
 
-            parent = node.parent
+            # Like the _fix_remove function, this is directly took from the
+            # CLRS book.
 
-            if node.left is self.nil: # takes care of the leaf case too
+            y = node
+            red = y.red
+
+            if node.left is self.nil:
+                x = node.right
                 self.transplant(node, node.right)
 
             elif node.right is self.nil:
+                x = node.left
                 self.transplant(node, node.left)
-                
+
             else:
-                
+
+                y = node.right
+
+                while y.left is not self.nil:
+
+                    y = y.left
+
+                red = y.red
+                x = y.right
+
+                if y.parent is node:
+                    x.parent = y
+
+                else:
+
+                    self.transplant(y, y.right)
+                    y.right = node.right
+                    y.right.parent = y
+
+                self.transplant(node, y)
+
+                y.left = node.left
+                y.left.parent = y
+                y.red = node.red
+
+            if red is False:
+                self._fix_remove(x)
+
+    # This is basically just a Python implementation of the CLRS algorithm
+    # to fix a RB tree deletion, almost no changes were made.
+    def _fix_remove(self, node):
+
+        while node is not self.root and node.is_black():
+
+            if node is node.parent.left:
+
+                sib = node.parent.right
+
+                # Case 1
+                if sib.is_red():
+
+                    sib.set_black()
+                    node.parent.set_red()
+                    self.rotate_left(node.parent)
+                    sib = node.parent.right
+
+                # Case 2
+                if sib.left.is_black() and sib.right.is_black():
+
+                    sib.set_red()
+                    node = node.parent
+
+                # Case 3
+                elif sib.right.is_black():
+
+                    sib.left.set_black()
+                    sib.set_red()
+                    self.rotate_right(sib)
+                    sib = node.parent.right
+
+                # Case 4
+                # It is mandatory to have another explicit "if" check here,
+                # even if CLRS does not suggest it.
+                elif sib.is_black() and sib.right.is_red():
+
+                    sib.red = node.parent.red
+                    node.parent.set_black()
+                    sib.right.set_black()
+                    self.rotate_left(node.parent)
+                    node = self.root
+
+            else:
+
+                sib = node.parent.left
+
+                # Case 5
+                if sib.is_red():
+
+                    sib.set_black()
+                    node.parent.set_red()
+                    self.rotate_right(node.parent)
+                    sib = node.parent.left
+
+                # Case 6
+                if sib.right.is_black() and sib.left.is_black():
+
+                    sib.set_red()
+                    node = node.parent
+
+                # Case 7
+                elif sib.left.is_black():
+
+                    sib.right.set_black()
+                    sib.set_red()
+                    self.rotate_left(sib)
+                    sib = node.parent.left
+
+                # Case 8
+                # Here holds the same observation made for case 4.
+                elif sib.is_black() and sib.left.is_red():
+
+                    sib.red = node.parent.red
+                    node.parent.set_black()
+                    sib.left.set_black()
+                    self.rotate_right(node.parent)
+                    node = self.root
+
+        node.set_black()
+
+    # This function and the _old_fix_remove one do not work properly and are
+    # there only to be possibly debugged for educational purposes.
+    def _old_remove(self, key):
+
+        node = self.root
+
+        while node is not self.nil and node.key != key:
+
+            if key < node.key:
+                node = node.left
+
+            else:   # key > node.key
+                node = node.right
+
+        if node is self.nil:
+            return None
+
+        else:
+
+            # Case 1
+            if node.left is not self.nil and node.right is not self.nil:
+
                 curr = node.right
 
                 while curr.left is not self.nil:
-                    
+
                     curr = curr.left
 
                 successor = curr
 
-                if successor.parent is node:
-                    successor.right.parent = successor
+                node.key = successor.key
+                node.data = successor.data
 
-                # TODO: finish implementing
-                    
+                node = successor
+
+            # Case 2
+            if node.left is not self.nil and node.right is self.nil:
+                child = node.left
+
+            # Case 3 and end of case 1 and also case 0 (no children)
+            else:   # node.left is self.nil or node.right is not self.nil
+                child = node.right
+
+            self.transplant(node, child)
+
+            if node.is_black():
+                self._old_fix_remove(child)
+
+            # Probably useless
+            # if node.parent is self.nil:
+            #     self.root = child
+
+    # This function and the _old_remove one do not work properly and are
+    # there only to be possibly debugged for educational purposes.
+    def _old_fix_remove(self, node):
+
+        while node is not self.root and node.is_black():
+
+            parent = node.parent
+
+            # Cases: 1 - 2 - 3 - 4
+            if node is parent.left:
+
+                sib = parent.right
+                slc = sib.left
+                src = sib.right
+
+                # Case 1
+                if sib.is_red():
+                    parent.set_red()
+                    sib.set_black()
+                    self.rotate_left(parent)
+
+                else:
+
+                    # Case 2
+                    if slc.is_black() and src.is_black():
+                        sib.set_red()
+                        node = parent
+
+                    # Case 3
+                    elif slc.is_red() and src.is_black():
+                        slc.set_black()
+                        parent.set_red()
+                        self.rotate_right(sib)
+
+                    # Case 4
+                    elif src.is_red():
+                        sib.red = parent.red
+                        parent.set_black()
+                        src.set_black()
+                        self.rotate_left(parent)
+                        node = self.root
+
+            # Cases: 5 - 6 - 7 - 8
+            else:
+
+                sib = parent.left
+                slc = sib.left
+                src = sib.right
+
+                # Case 5
+                if sib.is_red():
+                    parent.set_red()
+                    sib.set_black()
+                    self.rotate_right(parent)
+
+                else:
+
+                    # Case 6
+                    if slc.is_black() and src.is_black():
+                        sib.set_red()
+                        node = parent
+
+                    # Case 7
+                    elif slc.is_black() and src.is_red():
+                        src.set_black()
+                        parent.set_red()
+                        self.rotate_left(node)
+
+                    # Case 8
+                    elif slc.is_red():
+                        sib.red = parent.red
+                        parent.set_black()
+                        slc.set_black()
+                        self.rotate_right(parent)
+                        node = self.root
+
+        if node is not self.nil:
+            node.set_black()
+
+    # TODO: make it throw exceptions
+    def assert_validity(self):
+
+        if self.nil.is_red():
+            return False
+
+        if self._assert_validity(self.root) is None:
+            print("RB tree is not valid.")
+            return False
+
+        return True
+
+    def _assert_validity(self, node):
+
+        if node is self.nil:
+            return 1
+
+        if node.is_red() and (node.left.is_red() or node.right.is_red()):
+            print(
+                "Red node with key " +
+                str(node.key) +
+                " has at least one red children."
+            )
+            return None
+
+        if ((node.left is not self.nil and node.key < node.left.key) or
+                (node.right is not self.nil and node.key > node.right.key)):
+            print("BST violation at node with key " + str(node.key) + ".")
+            return None
+
+        lbh = self._assert_validity(node.left)
+        rbh = self._assert_validity(node.right)
+
+        if lbh is not None and rbh is not None and lbh == rbh:
+
+            if node.is_black():
+                return lbh + 1
+
+            else:
+                return lbh
+
+        else:
+            return None
+
+    def root_black_height(self):
+
+        return self.black_height(self.root)
+
+    def black_height(self, node):
+
+        return self._black_height(node, True)
+
+    def _black_height(self, node, is_first):
+
+        if node is self.nil:
+
+            if is_first:
+                return 0
+
+            else:
+                return 1
+
+        lbh = self._black_height(node.left, False)
+        rbh = self._black_height(node.right, False)
+
+        if lbh is not None and rbh is not None and lbh == rbh:
+
+            if node.is_black() and not is_first:
+                return lbh + 1
+
+            else:
+                return lbh
+
+        else:
+            return None
+
     def transplant(self, node, new_node):
 
         parent = node.parent
@@ -227,10 +533,10 @@ class RedBlackTree():
         if parent is self.nil:
             self.root = new_node
 
-        elif parent.left is node:
+        elif node is parent.left:
             parent.left = new_node
 
-        else: # parent.right is node
+        else:   # parent.right is node
             parent.right = new_node
 
         new_node.parent = parent
@@ -247,7 +553,12 @@ class RedBlackTree():
 
         if node is not self.nil:
 
-            print(prefix + ("└──" if is_tail else "├── ") + str(node.key))
+            node_type = "R" if node.is_red() else "B"
+
+            print(prefix +
+                  ("└──" if is_tail else "├── ") +
+                  str(node.key) +
+                  node_type)
 
             prefix_add = "    " if is_tail else "│   "
 
@@ -262,13 +573,50 @@ class RedBlackTree():
 rbtree = RedBlackTree()
 rbtree.add(10, 214)
 rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
 rbtree.add(14, 123)
 rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
 rbtree.add(235, 7)
 rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
 rbtree.add(9, "test")
 rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
 rbtree.add(98, "lol")
 rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
 rbtree.add(126)
 rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
+print("Start removing")
+rbtree.remove(10)
+rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
+rbtree.remove(9)
+rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
+rbtree.remove(126)
+rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
+rbtree.remove(98)
+rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
+rbtree.remove(235)
+rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
+rbtree.remove(14)
+rbtree.print_structure()
+print(rbtree.assert_validity())
+print()
